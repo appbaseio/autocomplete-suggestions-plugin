f5b93e1dd3b85beaebff0ebb5b1d6f3dab8a75de
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-06 15:41:45 +0530

    fix(example): fixed advanced-example and added result component to render along

--- examples/advanced-example/script.js
+++ examples/advanced-example/script.js
@@ -3,7 +3,6 @@ import { autocomplete } from "@algolia/autocomplete-js";
 import "@algolia/autocomplete-theme-classic";
 import createSuggestionsPlugin from "@appbaseio/autocomplete-suggestions-plugin";
 import { h } from "preact";
-import { getIcon } from "./utils";
 import { renderResults } from "./utils";
 
 var JSONTreeView = require("json-tree-view");
@@ -67,56 +66,33 @@ const advancedUsagePlugin = createSuggestionsPlugin(
   },
   {
     renderItem: (props) => {
-      const { item, setQuery, refresh } = props;
-      if (item.type === "index") {
-        return (
-          <a
-            className="aa-item product-item"
-            href={
-              item._source?.url ??
-              `https://www.google.com/search?q=${item.value}`
-            }
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="product-image">
-              <img src={item._source.image} alt={item.value} />
-            </div>
-            <div className="product-details">
-              <h4>{item.value}</h4>
-              <p>{item._source.shortDescription}</p>
-            </div>
-          </a>
-        );
-      }
+      const { item } = props;
       return (
-        <div className="item">
-          <div className="item__content-wrapper">
-            {getIcon(item.type)()}
-            <span
-              dangerouslySetInnerHTML={{
-                __html: item.value,
-              }}
-            ></span>
+        <a
+          className="aa-item product-item"
+          href={
+            item._source?.url ?? `https://www.google.com/search?q=${item.value}`
+          }
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="product-image">
+            <img
+              src={
+                item._source?.image ??
+                "https://m.media-amazon.com/images/I/81c83vd4O+L._SY879_.jpg"
+              }
+              alt={item.value}
+            />
           </div>
-          <div className="item__actions-wrapper">
-            {" "}
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-
-                setQuery(item.label.replace(/(<([^>]+)>)/gi, ""));
-                refresh();
-              }}
-              type="button"
-              className="set-search-arrow"
-            >
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M8 17v-7.586l8.293 8.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414l-8.293-8.293h7.586c0.552 0 1-0.448 1-1s-0.448-1-1-1h-10c-0.552 0-1 0.448-1 1v10c0 0.552 0.448 1 1 1s1-0.448 1-1z"></path>
-              </svg>
-            </button>
+          <div className="product-details">
+            <h4>{item.value} (Promoted)</h4>
+            <p>
+              {item._source?.shortDescription ??
+                "Samsung offers latest smartphones with advanced technology and design. Buy 3G, 4G, dual sim smartphone at best prices in India."}
+            </p>
           </div>
-        </div>
+        </a>
       );
     },
     onItemSelect: (props) => {
@@ -158,7 +134,7 @@ autocomplete({
   container: "#autocomplete",
   placeholder: "Search for products",
   openOnFocus: true,
-  // debug: true, //uncomment to keep the dropdown open
+  debug: true, //uncomment to keep the dropdown open
   plugins: [defaultUsagePlugin, advancedUsagePlugin],
   detachedMediaQuery: "none",
   onStateChange({ state: { context } }) {
