abc4b093752f4a4164168f94ecfa4bb84294faea
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-06 12:56:16 +0530

    fix(example): revert breaking change in basic example

--- examples/basic-example/script.js
+++ examples/basic-example/script.js
@@ -1,7 +1,20 @@
 import { autocomplete } from "@algolia/autocomplete-js";
 import "@algolia/autocomplete-theme-classic";
 import createSuggestionsPlugin from "@appbaseio/autocomplete-suggestions-plugin";
+import { renderResults } from "./utils";
 
+var JSONTreeView = require("json-tree-view");
+// appbase client config object
+const appbaseClientConfig = {
+  url: "https://appbase-demo-ansible-abxiydt-arc.searchbase.io",
+  app: "best-buy-dataset",
+  credentials: "b8917d239a52:82a2f609-6439-4253-a542-3697f5545947",
+  settings: {
+    userId: "s@s",
+    enableQueryRules: true,
+    recordAnalytics: true,
+  },
+};
 // reactivesearch api configuration
 const rsApiConfig = {
   size: 3,
