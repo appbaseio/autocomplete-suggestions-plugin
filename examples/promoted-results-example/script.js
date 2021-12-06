cf21ec2370686ed0db00f467fa13d5c645edef9a
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-06 13:11:11 +0530

    fix(example): fixed promoted-result-example and added result component to render along

--- examples/promoted-results-example/script.js
+++ examples/promoted-results-example/script.js
@@ -1,54 +1,56 @@
 import { autocomplete } from "@algolia/autocomplete-js";
 import "@algolia/autocomplete-theme-classic";
 import createSuggestionsPlugin from "@appbaseio/autocomplete-suggestions-plugin";
-
+import { renderResults } from "./utils";
+var JSONTreeView = require("json-tree-view");
 // appbase client config object
 const appbaseClientConfig = {
-  app: "recipes-demo",
-  credentials: "a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61",
   url: "https://appbase-demo-ansible-abxiydt-arc.searchbase.io",
+  app: "best-buy-dataset",
+  credentials: "b8917d239a52:82a2f609-6439-4253-a542-3697f5545947",
   settings: {
+    userId: "s@s",
+    enableQueryRules: true,
     recordAnalytics: true,
   },
 };
 
 // reactivesearch api configuration
 const rsApiConfig = {
+  size: 3,
+  highlight: true,
   dataField: [
     {
-      field: "title",
-      weight: 5,
+      field: "name.autosuggest",
+      weight: "1",
     },
     {
-      field: "title.search",
-      weight: 1,
-    },
-
-    {
-      field: "NER.keyword",
-      weight: 3,
+      field: "name",
+      weight: "3",
     },
   ],
-  highlight: true,
   enableRecentSuggestions: true,
   recentSuggestionsConfig: {
     size: 3,
     minHits: 2,
-    index: "recipes-demo",
+    index: "best-buy-dataset",
   },
   enablePopularSuggestions: true,
   popularSuggestionsConfig: {
     size: 3,
     minChars: 3,
-    index: "recipes-demo",
+    index: "best-buy-dataset",
   },
-  index: "recipes-demo",
-  size: 3,
+  index: "best-buy-dataset",
 };
 
-const suggestionsPlugin = createSuggestionsPlugin(appbaseClientConfig, {
-  ...rsApiConfig,
-});
+const suggestionsPlugin = createSuggestionsPlugin(
+  appbaseClientConfig,
+  {
+    ...rsApiConfig,
+  },
+  { useContextValue: true }
+);
 
 autocomplete({
   container: "#autocomplete",
@@ -56,5 +58,8 @@ autocomplete({
   // debug: true,
   openOnFocus: true,
   detachedMediaQuery: "none",
+  onStateChange({ state: { context } }) {
+    renderResults(context, JSONTreeView);
+  },
   // ...
 });
