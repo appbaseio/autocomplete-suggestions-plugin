e0713769d75d8587468a2a4ef994afa0afb64dfa
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-06 17:17:00 +0530

    fix(example): fixed category-field-example and added result component to render along

--- examples/category-field-example/script.js
+++ examples/category-field-example/script.js
@@ -1,16 +1,33 @@
 import { autocomplete } from "@algolia/autocomplete-js";
 import "@algolia/autocomplete-theme-classic";
 import createSuggestionsPlugin from "@appbaseio/autocomplete-suggestions-plugin";
+import { renderResults } from "./utils";
 
+var JSONTreeView = require("json-tree-view");
 // appbase client config object
 const appbaseClientConfig = {
   url: "https://appbase-demo-ansible-abxiydt-arc.searchbase.io",
   app: "best-buy-dataset",
-  credentials: "b8917d239a52:82a2f609-6439-4253-a542-3697f5545947"
+  credentials: "b8917d239a52:82a2f609-6439-4253-a542-3697f5545947",
+  settings: {
+    userId: "s@s",
+    enableQueryRules: true,
+    recordAnalytics: true,
+  },
 };
 
 // reactivesearch api configuration
 const rsApiConfig = {
+  dataField: [
+    {
+      field: "name.autosuggest",
+      weight: "1",
+    },
+    {
+      field: "name",
+      weight: "3",
+    },
+  ],
   enableRecentSuggestions: true,
   recentSuggestionsConfig: {
     size: 3,
@@ -25,18 +42,25 @@ const rsApiConfig = {
   },
   index: "best-buy-dataset",
   size: 3,
-  categoryField: "department.keyword"
+  categoryField: "department.keyword",
 };
 
-const suggestionsPlugin = createSuggestionsPlugin(appbaseClientConfig, {
-  ...rsApiConfig
-});
+const suggestionsPlugin = createSuggestionsPlugin(
+  appbaseClientConfig,
+  {
+    ...rsApiConfig,
+  },
+  { useContextValue: true }
+);
 
 autocomplete({
   container: "#autocomplete",
   plugins: [suggestionsPlugin],
   // debug: true,
   openOnFocus: true,
-  detachedMediaQuery: "none"
+  detachedMediaQuery: "none",
+  onStateChange({ state: { context } }) {
+    renderResults(context, JSONTreeView);
+  },
   // ...
 });
