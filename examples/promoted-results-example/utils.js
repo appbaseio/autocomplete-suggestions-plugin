fc3c19ddf96b134b32cc84dd343f3a186b3627f2
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-10 01:15:50 +0530

    fix: add another api call to fetch search type results for promoted example

--- examples/promoted-results-example/utils.js
+++ examples/promoted-results-example/utils.js
@@ -1,27 +1,57 @@
-export const renderResults = (context, JSONTreeView) => {
+export const renderResults = (
+  { value, url, app, credentials, settings, query },
+  JSONTreeView
+) => {
   const resultStatsElement = document.getElementById("results-stats");
   const resultElement = document.getElementById("result");
-  if (Object.keys(context).length) {
-    if (context.total) {
-      resultStatsElement.style.display = "block";
-      // setting results stats
-      resultStatsElement.innerHTML = `Found ${context.total} results in ${context.time} seconds`;
-    } else {
-      resultStatsElement.style.display = "none";
-    }
-    if (context.resultsJson) {
-      resultElement.style.display = "block";
-      resultElement.innerHTML = "";
-      var jsonView = new JSONTreeView("Results", context.resultsJson);
-      jsonView.readonly = true;
-      jsonView.expand(true);
-      var textnode = document.createTextNode("No Results Found");
-      // setting results json
-      resultElement.appendChild(
-        Object.keys(context.resultsJson).length ? jsonView.dom : textnode
-      );
-    } else {
-      resultElement.style.display = "none";
-    }
-  }
+
+  fetch(`${url}/${app}/_reactivesearch.v3`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Basic ${btoa(credentials)}`,
+    },
+    body: JSON.stringify({
+      settings,
+      query: [
+        {
+          id: "search",
+          type: "search",
+          ...query,
+          value,
+        },
+      ],
+    }), // body data type must match "Content-Type" header
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const responseData = data.search;
+
+      if (responseData?.hits?.total?.value) {
+        resultStatsElement.style.display = "block";
+        // setting results stats
+        resultStatsElement.innerHTML = `Displaying ${responseData?.hits?.hits.length} results, of ${responseData.hits.total.value} results found in ${responseData.took} ms`;
+      } else {
+        resultStatsElement.style.display = "none";
+      }
+      if (Array.isArray(responseData?.hits?.hits)) {
+        resultElement.style.display = "block";
+        resultElement.innerHTML = "";
+        var jsonView = new JSONTreeView("Results", [
+          ...responseData?.hits?.hits,
+        ]);
+        jsonView.readonly = true;
+        jsonView.expand(true);
+        var textnode = document.createTextNode("No Results Found");
+        // setting results json
+        resultElement.appendChild(
+          Object.keys(responseData?.hits?.hits).length ? jsonView.dom : textnode
+        );
+      } else {
+        resultElement.style.display = "none";
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
